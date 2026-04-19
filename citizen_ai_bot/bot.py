6016df66a01e8c581e5c9d7975ed81660f9f7b9a
codenovice-xero <c.rodgers@rpcmn.com>
2026-04-19 13:16:16 -0500

    Update bot.py

--- citizen_ai_bot/bot.py
+++ citizen_ai_bot/bot.py
@@ -6,6 +6,8 @@ import discord
 from discord.ext import commands
 
 from .config import settings
+from .services import StarCitizenService
+from .uex_client import UEXClient
 
 log = logging.getLogger(__name__)
 
@@ -14,6 +16,7 @@ class CitizenAIBot(commands.Bot):
     def __init__(self) -> None:
         intents = discord.Intents.default()
         super().__init__(command_prefix="!", intents=intents)
+        self.sc_service = StarCitizenService(UEXClient())
 
     async def setup_hook(self) -> None:
         await self.load_extension("citizen_ai_bot.cogs.starcitizen")
@@ -24,6 +27,12 @@ class CitizenAIBot(commands.Bot):
         else:
             synced = await self.tree.sync()
             log.info("Synced %s global commands", len(synced))
+
+    async def close(self) -> None:
+        try:
+            await self.sc_service.client.close()
+        finally:
+            await super().close()
 
     async def on_ready(self) -> None:
         log.info("Bot connected as %s", self.user)
